3b800b51f01b04f0ed389c8ec6ed96ef578703f4
nancycrojas <nanclarisaok@gmail.com>
2022-06-27 00:37:59 -0300

    Funciones - ejercicio 10

---
+++ app.js
@@ -0,0 +1,11 @@
+//saludar(nombre)
+//Crear una función saludar que tome como argumentos un nombre y devuelva un saludo que lo incluya.
+
+//saludar('Ada') // 'Hola Ada, un gusto conocerte'
+
+const saludar = (nombre) => {
+//function saludar(nombre) {
+     return ("Hola " + nombre + ", un  gusto conocerte");
+}
+
+saludar("Ada");
